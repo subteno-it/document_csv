9ca728e360785e453b0a1b7426335c641805a140
Christophe Chauvet <christophe.chauvet@syleam.fr>
2010-03-09 14:52:33 +0100

    [ADD] When select the field for the model, retrieve the fields in these inherits

--- object/ir_model.py
+++ object/ir_model.py
@@ -21,8 +21,28 @@
 #
 ##############################################################################
 
-import document
-import export
-import ir_model
+from osv import osv
+from osv import fields
+
+class ir_model_fields(osv.osv):
+    _inherit = 'ir.model.fields'
+
+    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=80):
+        """
+        Extend this method to retrieve the model and these inherits
+        """
+        if context.get('import'):
+            model_obj = self.pool.get('ir.model')
+            model = model_obj.read(cr, uid, args[0][2], ['model'])
+            inherits = self.pool.get(model['model'])._inherits
+            if inherits:
+                mod_ids = [args[0][2]]
+                for m in inherits:
+                    mod_id = model_obj.search(cr, uid, [('model','=', m)])
+                    mod_ids.append(mod_id[0])
+                args = [('model_id','in', mod_ids)]
+        return super(ir_model_fields, self).name_search(cr, uid, name, args, operator, context, limit)
+
+ir_model_fields()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
